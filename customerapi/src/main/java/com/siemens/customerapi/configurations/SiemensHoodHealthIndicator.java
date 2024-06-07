4bb1008e58560cffc30d27bc908471187d8d2057
Parameswari Bala <parameswaribala@gmail.com>
2024-06-07 17:14:39 +0530

    updated

--- customerapi/src/main/java/com/siemens/customerapi/configurations/SiemensHoodHealthIndicator.java
+++ customerapi/src/main/java/com/siemens/customerapi/configurations/SiemensHoodHealthIndicator.java
@@ -1,16 +1,34 @@
 package com.siemens.customerapi.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
 
 @Component
 @Endpoint(id="siemenshoodhealth")
 public class SiemensHoodHealthIndicator {
+    /*
     @ReadOperation
     @Bean
     public String message(){
         return "Message from Hood";
+    }
+*/
+    @Value("${datasource_validationquery}")
+    private String validationQuery;
+
+    @ReadOperation
+    @Bean("SmartDBHealthIndicator")
+    public HealthIndicator smartDBHealthIndicatior(@Qualifier("smartdb") @Autowired DataSource ds){
+
+        return new DataSourceHealthIndicator(ds,validationQuery);
     }
 }
