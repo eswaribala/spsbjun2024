df0ebc9ed4d47a027964624cc0c15249b4a817dc
Parameswari Bala <parameswaribala@gmail.com>
2024-06-07 14:53:52 +0530

    updated

--- customerapi/src/main/java/com/siemens/customerapi/controllers/CustomerFormController.java
+++ customerapi/src/main/java/com/siemens/customerapi/controllers/CustomerFormController.java
@@ -1,14 +1,23 @@
 package com.siemens.customerapi.controllers;
-
+import com.siemens.customerapi.models.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerFormController {
 
     @GetMapping("/home")
-    public String load(){
+    public String load(Model model){
+        model.addAttribute("customer", new Customer());
         return "home.html";
+    }
+    @PostMapping("/save")
+    public String save(@ModelAttribute("customer") Customer customer){
+        System.out.println(customer.getName().getFirstName());
+        return "showcustomer.html";
     }
 
 }
